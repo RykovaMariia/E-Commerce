fb592f7f8ae862aa50e722ee909aa197898d5c7d
Mariia Rykova <maryrykova24@gmail.com>
2025-05-11 23:19:53 +0400

    chore: setup eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  plugins: {
+    prettier: eslintPluginPrettier,
+  },
+  files: ['**/*.ts', '**/*.vue', '**/*.js'],
+  ignores: ['node_modules', 'dist', 'public', '**/*.md'],
+  rules: {
+    'prettier/prettier': 'error',
+    semi: ['error', 'never'],
+    'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/component-api-style': ['error', ['script-setup', 'composition']],
+    'vue/html-self-closing': ['error'],
+  },
+})
