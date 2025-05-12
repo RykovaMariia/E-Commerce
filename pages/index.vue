45505d96a5fda3ada39912083fc2d4fcfbf00cf8
VKTRenokh <viktorenokh@gmail.com>
2025-05-12 11:18:15 +0300

    feat: add links

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    ASDF
+    <ULink as="button">Sign in</ULink>
+    <ULink as="button">Sign up</ULink>
+  </div>
+</template>
+
+<style scoped></style>
