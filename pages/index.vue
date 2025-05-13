2b8ae2de82533614ba9273dac1624ae800c63e0a
VKTRenokh <viktorenokh@gmail.com>
2025-05-13 11:27:36 +0300

    fix: remove useless text

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,19 @@
 <template>
   <div>
     <Head>
-      <Meta name="description"
-        >Discover premium _ supplies from trusted global brands</Meta
-      >
+      <Meta name="description">Discover premium _ supplies from trusted global brands</Meta>
       <Title>?</Title>
     </Head>
-
-    ASDF
-    <ULink as="button" to="/login">Sign in</ULink>
-    <ULink as="button" to="/register">Sign up</ULink>
+    <ULink
+      as="button"
+      to="/login"
+      >Sign in</ULink
+    >
+    <ULink
+      as="button"
+      to="/register"
+      >Sign up</ULink
+    >
   </div>
 </template>
 
